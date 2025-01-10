555993146dfe6a186d199ddfe2ee7b66db219d3a
ahmad-cod <ahmadadewale800@gmail.com>
2025-01-10 18:03:08 +0100

    Implement user authentication and CRUD actions for expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,26 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @expenses = current_user.expenses.order(logged_date: :desc)
+  end
+
+  def new
+    @expense = current_user.expenses.build
+  end
+
+  def create
+    @expense = current_user.expenses.build(expense_params)
+
+    if @expense.save
+      redirect_to expenses_path, notice: 'Expense was successfully logged.'
+    else
+      render :new, alert: 'Error logging expense.'
+    end
+  end
+
+  def edit
+    @expense = current_user.expenses.find(params[:id])
+  end
+
 end
